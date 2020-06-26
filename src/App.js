6103055d2a084c5c0d83cfd5ebe4a552544c7a41
shanemoyo <smoyo@pdx.edu>
2020-06-26 14:24:22 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
+import { 
+  BrowserRouter as Router,
+  Route, 
+  Switch, 
+  Link
+} from 'react-router-dom'; 
 import FoodContainer from './component/Food'; 
+
+const About = () => (
+  <section>
+    <h1>Welcome to my food api wrapper thing</h1>
+    <p>Find a recipe by searching a food</p>
+  </section> 
+)
 
 function App() {
   return (
     <div className="App">
-      <FoodContainer /> 
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Switch>
+          <Route path="/about" render={() => <About/>}/>
+          <Route path="/" component={FoodContainer}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
