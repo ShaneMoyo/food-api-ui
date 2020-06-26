4bb76e7750eb589e8c1ec251be658709a3551603
shanemoyo <smoyo@pdx.edu>
2020-06-26 16:05:14 -0700

    new api added

--- src/services/foodAPI.js
+++ src/services/foodAPI.js
@@ -10,6 +10,19 @@ export default {
         } catch(error) {
             console.log('error: ', error); 
         }
+    }, 
+    async getRecipeSummaryByUrl(url) {
+        try {
+            const response = await fetch(
+                `https://api.spoonacular.com/recipes/extract?url=${url}&apiKey=${process.env.REACT_APP_API_KEY}`
+                
+            ).then(res => res.json());
+            console.log('response: ', response); 
+            return response.results; 
+        } catch(error) {
+            console.log('error: ', error); 
+        }
     }
 }
 
+https://api.spoonacular.com/recipes/extract?url=https://foodista.com/recipe/ZHK4KPB6/chocolate-crinkle-cookies
